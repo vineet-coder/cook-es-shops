2a6a57c422f66c0156796fda35eef3eea59efee1
vineet-coder <vineetsinghal2312@gmail.com>
2021-04-12 21:23:26 +0530

    changes in cart

--- src/pages/Cart.js
+++ src/pages/Cart.js
@@ -71,25 +71,7 @@ export const Cart = () => {
           </div>
         </div>
       </div>
-      <footer className="footer">
-        <ul className="footer-list">
-          <li className="footer-list-item">
-            <SiGmail />
-          </li>
-          <li className="footer-list-item">
-            <FiInstagram />
-          </li>
-          <li className="footer-list-item">
-            <AiOutlineTwitter />
-          </li>
-        </ul>
-
-        <img
-          src="./images/susie-ilus-header.png"
-          alt="fotterImg"
-          className="footer-img"
-        />
-      </footer>
+      <Footer />
     </>
   );
 };
@@ -161,7 +143,6 @@ const CartCard = ({ item }) => {
           </div>
         </div>
       </div>
-      <Footer />
     </>
   );
 };
